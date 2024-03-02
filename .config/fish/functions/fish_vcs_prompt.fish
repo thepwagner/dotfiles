096bbc0d7a3e6eb57c72c6b775f3c2c5dada4829
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2024-03-02 06:22:59 -0500

    fish repo: not basename

--- .config/fish/functions/fish_vcs_prompt.fish
+++ .config/fish/functions/fish_vcs_prompt.fish
@@ -1,6 +1,6 @@
 function fish_vcs_prompt --description 'Print the prompts for all available vcsen'
-    set IGNORED_REPOS mycloud
-    if not contains (basename $PWD) $IGNORED_REPOS
+    set IGNORED_REPOS {$HOME}/mycloud
+    if not contains $PWD $IGNORED_REPOS
         fish_git_prompt $argv
     end
 end
